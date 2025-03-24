07a66b44d7d2682e5647dfffa5682bc5b7194498
pagatkuro <pagat.kuro@gmail.com>
2025-03-24 18:25:28 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 import { connect } from "cloudflare:sockets";
 
 // Variables
-const rootDomain = "pagat-kuro.workers.dev"; // Ganti dengan domain utama kalian
+const rootDomain = "vishiki.web.id"; // Ganti dengan domain utama kalian
 const serviceName = "vishikuro"; // Ganti dengan nama workers kalian
 const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = ""; // Ganti dengan email yang kalian gunakan
